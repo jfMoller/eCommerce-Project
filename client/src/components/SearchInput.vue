06bddb7de626b16ddd5e77698b64dd34a5f74dbd
jm <johan.moller040@gmail.com>
2023-12-28 12:26:55 +0100

    added search filters for product searching - frontend

--- client/src/components/SearchInput.vue
+++ client/src/components/SearchInput.vue
@@ -1,31 +1,56 @@
 <template>
-  <div class="w-full flex justify-center items-center">
-    <input type="text" v-model="searchInput"
-      class="max-w-md px-4 py-2 mb-6 rounded-md border border-gray-300 focus:outline-none w-full"
+  <div class="w-full flex flex-col justify-center items-center relative">
+    <div class="flex justify-center items-center bg-white border border-gray-300 rounded px-3 w-[28rem]">
+      <i class="fas fa-search text-gray-500"></i>
+    <input type="text" v-model="searchInput" @keyup.enter="handleSearch"
+      class="max-w-md px-4 py-2 w-full focus:aria-none"
       :placeholder="placeholder" />
+    </div>
+    <div
+      class=" bg-white w-[28rem] rounded-sm min-h-max shadow-md border border-gray-300 absolute top-[2.68rem] flex flex-col p-4 space-y-2">
+      <h3 class="text-xl font-semibold">Filter</h3>
+      <div class="w-full border bordet-t-gray-300"></div>
+      <label>
+        <input type="checkbox" v-model="filters.lowestPrice" @change="handleFilterChange" />
+        Lowest Price
+      </label>
+      <label>
+        <input type="checkbox" v-model="filters.highestPrice" @change="handleFilterChange" />
+        Highest price
+      </label>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
 
 export default defineComponent({
-  name: "SearchInput",
+  name: 'SearchInput',
   props: {
     placeholder: {
       type: String,
-      default: "Search...",
+      default: 'Search our product',
     },
   },
 
   setup(props, { emit }) {
     const searchInput = ref<string>('');
-
-    watch(searchInput, () => {
-      emit('search', searchInput.value);
+    const isOpenDropdown = ref<boolean>(false);
+    const filters = reactive({
+      lowestPrice: false,
+      highestPrice: false,
     });
 
-    return { searchInput };
-  }
+    function handleSearch() {
+      emit('search', searchInput.value);
+    }
+
+    function handleFilterChange() {
+      emit('search', { query: searchInput.value, filters });
+    }
+
+    return { searchInput, isOpenDropdown, filters, handleSearch, handleFilterChange };
+  },
 });
 </script>
