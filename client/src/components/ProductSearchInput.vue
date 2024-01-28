c5f3c8b01164c86620e2101e0625b06c3c6801b4
jm <johan.moller040@gmail.com>
2024-01-28 09:20:52 +0100

    fixed search input drop-shadow

--- client/src/components/ProductSearchInput.vue
+++ client/src/components/ProductSearchInput.vue
@@ -2,14 +2,14 @@
   <div :class="['flex flex-col justify-center items-center relative', additionalClass]" @mouseover="showDropdown"
     @mouseleave="hideDropdown" @keyup.enter="handleSearch">
     <div
-      class="flex justify-center items-center bg-white border border-white md:border-gray-600 rounded py-[0.6rem] md:py-0 px-6 sm:px-3 w-screen sm:w-full lg:w-[28rem]">
+      class="flex justify-center items-center bg-white border border-white md:border-gray-600 rounded py-[0.5rem] md:py-0 px-6 sm:px-3 w-screen sm:w-full lg:w-[28rem]">
       <i class="fas fa-search text-black"></i>
       <input type="text" v-model="searchInput" class="px-4 py-2 w-full focus:aria-black focus:outline-none"
         :placeholder="props.placeholder" />
       <i v-if="hasCloseSearchEnabled" class="fas fa-close cursor-pointer" @click="handleCloseSearch" />
     </div>
     <div v-if="isOpenDropdown"
-      class=" bg-white w-full lg:w-[28rem] transition duration-400 rounded-sm min-h-max shadow-md border border-gray-300 absolute top-[3.8rem] md:top-[2.6rem] lg:top-[2.63rem] flex flex-col p-4 space-y-2">
+      class=" bg-white w-full lg:w-[28rem] transition duration-400 rounded-sm min-h-max shadow-md border border-gray-300 absolute top-[3.73rem] md:top-[2.6rem] lg:top-[2.63rem] flex flex-col p-4 space-y-2">
       <h3 class="text-base font-semibold">SORT BY</h3>
       <div class="w-full border bordet-t-gray-300"></div>
       <div class="flex flex-col text-base items-start justify-center space-y-1">
@@ -123,3 +123,4 @@ export default defineComponent({
   },
 });
 </script>
+
