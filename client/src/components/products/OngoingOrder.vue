226cd932945d0d42388b23911fa892d8e7c725fe
jm <johan.moller040@gmail.com>
2024-02-11 18:19:05 +0100

    Reworked delivery details

--- client/src/components/products/OngoingOrder.vue
+++ client/src/components/products/OngoingOrder.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center">
-    <h2 class="text-l font-bold mb-6 text-left uppercase">Your Shopping Cart</h2>
+    <h2 class="text-l font-bold text-left uppercase w-full sm:min-w-max sm:w-[70%] py-4">Inspect your cart</h2>
     <div class="p-0 sm:p-4 bg-white rounded shadow w-full sm:min-w-max sm:w-[70%] mb-4">
       <LoadingScreen />
 
@@ -48,41 +48,64 @@
     </div>
 
     <div v-if="isAuthenticated" class="sm:min-w-max sm:w-[70%] w-full space-y-4">
-
+      <h2 class="text-l font-bold text-left uppercase">2. Fill in your delivery details</h2>
       <div class="p-4 bg-white rounded shadow">
-        <h2 class="text-l font-bold mb-6 text-left uppercase">2. Select delivery address</h2>
-        <div class="mb-4">
-          <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Delivery Address</label>
-          <input v-model="deliveryAddress" type="map" id="email"
-            class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">First name</label>
+        <input v-model="deliveryDetails.firstName" type="text" id="firstName"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Last name</label>
+        <input v-model="deliveryDetails.lastName" type="text" id="lastName"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Delivery Address</label>
+        <input v-model="deliveryDetails.address" type="text" id="address"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Post code</label>
+        <input v-model="deliveryDetails.postCode" type="text" id="postCode"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">City</label>
+        <input v-model="deliveryDetails.city" type="text" id="city"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Country code</label>
+        <input v-model="deliveryDetails.countryCode" type="text" id="countryCode"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Mobile</label>
+        <input v-model="deliveryDetails.mobile" type="text" id="mobile"
+          class="w-full px-3 py-2 border rounded border-gray-400 focus:outline-none" required />
+      </div>
+
+      <div class="flex justify-between">
+        <div class="p-4 bg-white rounded shadow w-full mr-4">
+          <h2 class="text-l font-bold mb-6 text-left uppercase">3. Select delivery method</h2>
+          <div class="mb-4">
+            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Delivery Method</label>
+            <select v-model="selectedDeliveryMethod" id="selectedDeliveryMethod">
+              <option v-for="deliveryMethod in deliveryMethods" :key="deliveryMethod" :value="deliveryMethod">
+                {{ deliveryMethod }}</option>
+            </select>
+          </div>
+        </div>
+
+
+        <div class="p-4 bg-white rounded shadow w-full">
+          <h2 class="text-l font-bold mb-6 text-left uppercase">4. Select payment method</h2>
+          <div class="mb-4">
+            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Payment Method</label>
+            <select v-model="selectedPaymentMethod" id="selectedDeliveryMethod">
+              <option v-for="paymentMethod in paymentMethods" :key="paymentMethod" :value="paymentMethod">
+                {{ paymentMethod }} </option>
+            </select>
+          </div>
         </div>
       </div>
 
       <div class="p-4 bg-white rounded shadow">
-        <h2 class="text-l font-bold mb-6 text-left uppercase">3. Select delivery method</h2>
-        <div class="mb-4">
-          <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Delivery Method</label>
-          <select v-model="selectedDeliveryMethod" id="selectedDeliveryMethod">
-            <option v-for="deliveryMethod in deliveryMethods" :key="deliveryMethod" :value="deliveryMethod">
-              {{ deliveryMethod }}</option>
-          </select>
-        </div>
-      </div>
-
-
-      <div class="p-4 bg-white rounded shadow">
-        <h2 class="text-l font-bold mb-6 text-left uppercase">4. Select payment method</h2>
-        <div class="mb-4">
-          <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Payment Method</label>
-          <select v-model="selectedPaymentMethod" id="selectedDeliveryMethod">
-            <option v-for="paymentMethod in paymentMethods" :key="paymentMethod" :value="paymentMethod">
-              {{ paymentMethod }} </option>
-          </select>
-        </div>
-      </div>
-
-      <div class="p-4 bg-white rounded shadow">
-        <h2 class="text-l font-bold my-6 text-left uppercase">{{ isAuthenticated ? '5' : '2' }}. Confirm your order</h2>
+        <h2 class="text-l font-bold mb-6 text-left uppercase">{{ isAuthenticated ? '5' : '2' }}. Confirm your order</h2>
         <div class="w-full flex justify-center items-center">
           <button @click="placeOrder" class="bg-green-500 hover:bg-green-600 px-6 py-2 text-l text-white rounded">Confirm
             order</button>
@@ -109,7 +132,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, ref, watch } from 'vue';
+import { computed, defineComponent, onMounted, reactive, ref, watch } from 'vue';
 import LoadingScreen from '../LoadingScreen.vue';
 import { useOrderStore } from '@/stores/network/orderStore';
 import { useShoppingCartStore } from '@/stores/shoppingCartStore';
@@ -129,8 +152,17 @@ export default defineComponent({
     const deliveryMethods = ref<DeliveryMethod[] | []>()
     const paymentMethods = ref<PaymentMethod[] | []>()
 
+    const deliveryDetails = reactive({
+      firstName: 'a',
+      lastName: 'b',
+      address: 'c',
+      postCode: 'd',
+      city: 'e',
+      countryCode: 'f',
+      mobile: 'g'
+    })
+
     const selectedDeliveryMethod = ref<string>('')
-    const deliveryAddress = ref<string>('')
     const selectedPaymentMethod = ref<string>('')
 
     onMounted(() => {
@@ -171,7 +203,7 @@ export default defineComponent({
     async function placeOrder() {
       await orderStore.API.placeOrder(
         selectedDeliveryMethod.value,
-        deliveryAddress.value,
+        deliveryDetails.address,
         selectedPaymentMethod.value)
       ongoingOrder.value = null
     }
@@ -182,7 +214,7 @@ export default defineComponent({
       removeProduct,
       deliveryMethods,
       selectedDeliveryMethod,
-      deliveryAddress,
+      deliveryDetails,
       paymentMethods,
       selectedPaymentMethod,
       placeOrder,
